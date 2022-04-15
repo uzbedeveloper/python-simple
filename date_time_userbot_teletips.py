3f20218571f83530c8d00a21ef3fa51f89fa6832
uzbedeveloper <103754533+uzbedeveloper@users.noreply.github.com>
2022-04-16 02:59:33 +0500

    Update date_time_userbot_teletips.py

--- date_time_userbot_teletips.py
+++ date_time_userbot_teletips.py
@@ -50,7 +50,7 @@ async def main_teletips():
                 except Exception:
                     pass        
                 print("Profile Updated! Murodhonov")
-            await asyncio.sleep(1)     
+            await asyncio.sleep(5e-3)     
     except FloodWait as e:
         await asyncio.sleep(e.x)         
 
