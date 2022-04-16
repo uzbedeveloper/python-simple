bd57809c05a8b43b26aee59958074d35b1edeb9d
uzbedeveloper <103754533+uzbedeveloper@users.noreply.github.com>
2022-04-16 13:45:09 +0500

    Update date_time_userbot_teletips.py

--- date_time_userbot_teletips.py
+++ date_time_userbot_teletips.py
@@ -51,7 +51,7 @@ async def main_teletips():
                 thrs = tsecs/(60*60)
                 
              
-                await Date_Time_Userbot_teletips.update_profile(bio = f"Tug'ulgan kunimga: {int(tdays)}-kun, {int(thrs)}-soat, {int(tmins)}-daqiqa qoldi" , last_name = f" ")
+                await Date_Time_Userbot_teletips.update_profile(bio = f" " , last_name = f" ")
                 await Date_Time_Userbot_teletips.set_profile_photo(photo="Image_final_teletips.jpg")
                 me = await Date_Time_Userbot_teletips.get_me()
                 photos = await Date_Time_Userbot_teletips.get_profile_photos("me")
